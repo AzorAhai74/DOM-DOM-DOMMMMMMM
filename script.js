ecbb61b0635bec311cd56ae4f0bdaf5785f606f9
Byron Baldwin <byronsbaldwin@gmail.com>
2019-07-13 22:15:52 -0500

    dom progress

--- script.js
+++ script.js
@@ -3,19 +3,37 @@ window.addEventListener('DOMContentLoaded', function() {
     let buttonOneText = document.createTextNode('Add Square');
     buttonOne.appendChild(buttonOneText);
     document.body.appendChild(buttonOne);
-    
+
+    let flexContainer = document.createElement('div');
+    flexContainer.className = 'flex-container';
+    document.body.appendChild(flexContainer);
+
+    let newSquare = 1;
 
     buttonOne.addEventListener('click', function() {
     let div = document.createElement('div');
     div.ClassName = 'squares';
     let div1 = document.getElementsByClassName('squares');
     div.class = div1;
-    div.id = 'newSquareId';
-        
+    div.id = (newSquare);
+    
     document.body.appendChild(div);    
     div.classList.add('squares');
-
+    div.style.cssFloat = 'right';
+    divSquareListeners(div);
+    
+    newSquare++;
+        
     });
+
+    function divSquareListeners(div) {
+        div.addEventListener('mouseover', function(){
+            
+        })
+    }
+
+
+    
     
     
 });
