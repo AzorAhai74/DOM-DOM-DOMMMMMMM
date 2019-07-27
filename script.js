9063702be8644ba632061ca57a927c5717ee43c3
Byron Baldwin <byronsbaldwin@gmail.com>
2019-07-26 22:21:11 -0500

    Dom progess

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ window.addEventListener('DOMContentLoaded', function() {
 
     let newSquare = 1;
 
+    let colors = ['blue', 'red', 'green', 'yellow', 'orange', 'brown', 'purple', 'pink'];
+
     buttonOne.addEventListener('click', function() {
     let div = document.createElement('div');
     div.ClassName = 'squares';
@@ -20,16 +22,46 @@ window.addEventListener('DOMContentLoaded', function() {
     document.body.appendChild(div);    
     div.classList.add('squares');
     div.style.cssFloat = 'right';
+    div.style.textAlign = 'center';
+    div.style.height = '100px';
+    div.style.lineHeight = '100px';
+    div.style.webkitTextFillColor = 'white';
     divSquareListeners(div);
-    
+
     newSquare++;
-        
+
+    
     });
+
+    function randomColors() {
+        let randomColor1 = colors[Math.floor(Math.random() * 7) + 1];
+        let randomColor2 = colors[Math.floor(Math.random() * 7) + 1];
+        let randomColor3 = colors[Math.floor(Math.random() * 7) + 1];
+        let randomColor4 = colors[Math.floor(Math.random() * 7) + 1];
+        let randomColor5 = colors[Math.floor(Math.random() * 7) + 1];
+        let randomColor6 = colors[Math.floor(Math.random() * 7) + 1];
+        let randomColor7 = colors[Math.floor(Math.random() * 7) + 1];
+        let randomColor8 = colors[Math.floor(Math.random() * 7) + 1];
+
+        let randomColors = '(' + randomColor1 + ',' + randomColor2 + ',' + randomColor3 + ',' + randomColor4 + ',' + randomColor5 + ',' + randomColor6 + ',' + randomColor7 + ',' + randomColor8 + ')';
+        console.log(randomColors);
+        return randomColors;        
+    }
 
     function divSquareListeners(div) {
         div.addEventListener('mouseover', function(){
-            
-        })
+            let textId = document.createTextNode(div.id.toString());
+            div.appendChild(textId); 
+        });
+
+        div.addEventListener('mouseout', function(){
+            div.removeChild(div.firstChild);
+        });
+
+        div.addEventListener('click', function() {
+            div.style.backgroundColor = randomColors();
+        });
+
     }
 
 
